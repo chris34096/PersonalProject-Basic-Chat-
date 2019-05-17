b1f884665597b1931a33f3bb4db9c615a0cd76d6
Christofer Perez <christoferperez@gmail.com>
2019-05-17 11:23:54 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const server = http.Server(app);
+const io = require("socket.io")(server);
+const users = [];
+
+server.listen(3333, function() {
+  console.log("The developpement of server 3333 is running");
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/styles/index.css", function(req, res) {
+  res.sendFile(__dirname + "/styles/index.css");
+});
+
+io.on("connection", function(socket) {
+  const name = "";
+
+  socket.on("has connected", function(username) {
+    name = username;
+    users.push(username);
+    io.emit("has connected", { username: username, usersList: users });
+  });
+
+  socket.on("disconnect", function() {
+    users.splice(users.indexOf(name), 1);
+    io.emit("has disconnected", { username: name, usersList, users });
+  });
+
+  socket.on("new message", function(data) {
+    io.emit("new message", data);
+  });
+});
---
+++ test.js
@@ -0,0 +1,7 @@
+const test = require("socket.io");
+
+if (test) {
+  console.log("It is there");
+} else {
+  console.log("It is not there");
+}
